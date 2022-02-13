94faff5ba513b6b1d6256f7116d68a3b08b51b24
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-02-13 23:43:23 +0000

    tape port

--- lib/src/tape.dart
+++ lib/src/tape.dart
@@ -1,3 +1,7 @@
+import 'package:tensorflow_wasm/src/engine.dart';
+import 'package:tensorflow_wasm/src/tensor.dart';
+import 'package:tensorflow_wasm/src/util_base.dart' as util;
+
 /**
  * @license
  * Copyright 2017 Google LLC. All Rights Reserved.
@@ -15,23 +19,46 @@
  * =============================================================================
  */
 
-import {Tensor} from './tensor';
-import {NamedTensorMap} from './tensor_types';
-import * as util from './util';
+// import {Tensor} from './tensor';
+// import {NamedTensorMap} from './tensor_types';
+// import * as util from './util';
 
-export interface TapeNode {
-  id: number;
-  kernelName: string;
-  outputs: Tensor[];
-  inputs: NamedTensorMap;
+class TapeNode {
+  final int id;
+  final String kernelName;
+  final List<Tensor> outputs;
+  final NamedTensorMap inputs;
   // Optional params, defined only for ops with gradient impl.
-  gradient?: (dys: Tensor[]) => NamedGradientMap;
-  saved?: Tensor[];
+  NamedGradientMap Function(List<Tensor> dys)? gradient;
+  final List<Tensor>? saved;
+
+  TapeNode({
+    required this.id,
+    required this.kernelName,
+    required this.outputs,
+    required this.inputs,
+    this.gradient,
+    this.saved,
+  });
+
+  TapeNode copyWith({
+    int? id,
+    String? kernelName,
+    List<Tensor>? outputs,
+    NamedTensorMap? inputs,
+  }) {
+    return TapeNode(
+      id: id ?? this.id,
+      kernelName: kernelName ?? this.kernelName,
+      outputs: outputs ?? this.outputs,
+      inputs: inputs ?? this.inputs,
+      gradient: this.gradient,
+      saved: this.saved,
+    );
+  }
 }
 
-export type NamedGradientMap = {
-  [inputName: string]: () => Tensor;
-};
+typedef NamedGradientMap = Map<String, Tensor Function()>;
 
 /**
  * Computes a list of TapeNodes that connect x to y, filtering everything else
@@ -41,26 +68,26 @@ export type NamedGradientMap = {
  * @param xs The input Tensors.
  * @param y The output Tensor.
  */
-export function getFilteredNodesXToY(
-    tape: TapeNode[], xs: Tensor[], y: Tensor): TapeNode[] {
+List<TapeNode> getFilteredNodesXToY(
+    List<TapeNode> tape, List<Tensor> xs, Tensor y) {
   // Forward pass to compute all the nodes and Tensors that are transitively a
   // function of x.
-  const tensorsFromX: {[tensorId: number]: boolean} = {};
-  const nodesFromX: {[nodeId: number]: boolean} = {};
-  for (let i = 0; i < xs.length; i++) {
+  final tensorsFromX = <int, bool>{};
+  final nodesFromX = <int, bool>{};
+  for (int i = 0; i < xs.length; i++) {
     tensorsFromX[xs[i].id] = true;
   }
 
-  for (let i = 0; i < tape.length; i++) {
-    const node = tape[i];
-    const nodeInputs = node.inputs;
-    for (const inputName in nodeInputs) {
-      const input = nodeInputs[inputName];
+  for (int i = 0; i < tape.length; i++) {
+    final node = tape[i];
+    final nodeInputs = node.inputs;
+    for (final inputName in nodeInputs.keys) {
+      final input = nodeInputs[inputName];
 
-      let anyInputFromX = false;
-      for (let j = 0; j < xs.length; j++) {
-        if (tensorsFromX[input.id]) {
-          node.outputs.forEach(output => tensorsFromX[output.id] = true);
+      bool anyInputFromX = false;
+      for (int j = 0; j < xs.length; j++) {
+        if (tensorsFromX[input?.id] == true) {
+          node.outputs.forEach((output) => tensorsFromX[output.id] = true);
           anyInputFromX = true;
           nodesFromX[node.id] = true;
           break;
@@ -74,19 +101,19 @@ export function getFilteredNodesXToY(
   }
 
   // Backward pass to find all of the nodes and Tensors that lead to y.
-  const tensorsLeadToY: {[tensorId: number]: boolean} = {};
+  final tensorsLeadToY = <int, bool>{};
   tensorsLeadToY[y.id] = true;
-  const nodesToY: {[nodeId: number]: boolean} = {};
+  final nodesToY = <int, bool>{};
 
-  for (let i = tape.length - 1; i >= 0; i--) {
-    const node = tape[i];
-    const nodeInputs = node.inputs;
+  for (int i = tape.length - 1; i >= 0; i--) {
+    final node = tape[i];
+    final nodeInputs = node.inputs;
 
     // If any of the outputs lead to y, mark all of the inputs as leading to y.
-    for (let j = 0; j < node.outputs.length; j++) {
-      if (tensorsLeadToY[node.outputs[j].id]) {
-        for (const inputName in nodeInputs) {
-          tensorsLeadToY[nodeInputs[inputName].id] = true;
+    for (int j = 0; j < node.outputs.length; j++) {
+      if (tensorsLeadToY[node.outputs[j].id] == true) {
+        for (final inputName in nodeInputs.keys) {
+          tensorsLeadToY[nodeInputs[inputName]!.id] = true;
           nodesToY[node.id] = true;
         }
         break;
@@ -95,26 +122,24 @@ export function getFilteredNodesXToY(
   }
 
   // Return the paths that come from x and lead to y.
-  const filteredTape: TapeNode[] = [];
-  for (let i = 0; i < tape.length; i++) {
-    const node = tape[i];
+  final filteredTape = <TapeNode>[];
+  for (int i = 0; i < tape.length; i++) {
+    final node = tape[i];
 
-    if (nodesFromX[node.id] && nodesToY[node.id]) {
+    if (nodesFromX[node.id] == true && nodesToY[node.id] == true) {
       // Prune the inputs from the node that aren't a function of x.
-      const prunedInputs: {[inputName: string]: Tensor} = {};
-      for (const inputName in node.inputs) {
-        const nodeInput = node.inputs[inputName];
-        if (tensorsFromX[nodeInput.id]) {
+      final prunedInputs = <String, Tensor>{};
+      for (final inputName in node.inputs.keys) {
+        final nodeInput = node.inputs[inputName]!;
+        if (tensorsFromX[nodeInput.id] == true) {
           prunedInputs[inputName] = nodeInput;
         }
       }
 
       // Copy the node and overwrite inputsAndArgs to the pruned version.
-      const prunedNode = Object.assign({}, node);
-      prunedNode.inputs = prunedInputs;
-      prunedNode.outputs = node.outputs;
+      final prunedNode = node.copyWith(inputs: prunedInputs);
 
-      filteredTape.push(prunedNode);
+      filteredTape.add(prunedNode);
     }
   }
 
@@ -128,63 +153,60 @@ export function getFilteredNodesXToY(
  * is mutated by this method.
  * @param filteredTape The filtered TapeNodes to backprop through.
  */
-export function backpropagateGradients(
-    tensorAccumulatedGradientMap: {[tensorId: number]: Tensor},
-    filteredTape: TapeNode[], tidy: (f: Function) => Tensor,
-    add: (a: Tensor, b: Tensor) => Tensor) {
+void backpropagateGradients(
+    Map<int, Tensor> tensorAccumulatedGradientMap,
+    List<TapeNode> filteredTape,
+    Tensor Function(Function f) tidy,
+    Tensor Function(Tensor, Tensor) add) {
   // Walk the tape backward and keep a map of Tensor to its gradient.
-  for (let i = filteredTape.length - 1; i >= 0; i--) {
-    const node = filteredTape[i];
+  for (int i = filteredTape.length - 1; i >= 0; i--) {
+    final node = filteredTape[i];
 
-    const dys: Tensor[] = [];
-    node.outputs.forEach(o => {
-      const gradTensor = tensorAccumulatedGradientMap[o.id];
+    final dys = <Tensor?>[]; // TODO: wasn't null
+    node.outputs.forEach((o) {
+      final gradTensor = tensorAccumulatedGradientMap[o.id];
       if (gradTensor != null) {
-        dys.push(gradTensor);
+        dys.add(gradTensor);
       } else {
         // This particular output is not in the back-propagation subgraph, so it
         // does not affect the final output, thus we put null for its dy.
-        dys.push(null);
+        dys.add(null);
       }
     });
 
     if (node.gradient == null) {
-      throw new Error(
-          `Cannot compute gradient: gradient function not found ` +
-          `for ${node.kernelName}.`);
+      throw Exception('Cannot compute gradient: gradient function not found ' +
+          'for ${node.kernelName}.');
     }
 
     // Backprop dy through this node and accumulate gradients over the inputs.
-    const inputGradients = node.gradient(dys);
+    final inputGradients = node.gradient!(dys.cast()); // TODO:
 
-    for (const inputName in node.inputs) {
-      if (!(inputName in inputGradients)) {
-        throw new Error(
-            `Cannot backprop through input ${inputName}. ` +
-            `Available gradients found: ${Object.keys(inputGradients)}.`);
+    for (final inputName in node.inputs.keys) {
+      if (!inputGradients.containsKey(inputName)) {
+        throw Exception('Cannot backprop through input ${inputName}. ' +
+            'Available gradients found: ${inputGradients.keys}.');
       }
 
       // Call the gradient function.
-      const dx = tidy(() => inputGradients[inputName]());
-      if (dx.dtype !== 'float32') {
-        throw new Error(
-            `Error in gradient for op ${
-                node.kernelName}. The gradient of input ` +
-            `${inputName} must have 'float32' dtype, but has '${dx.dtype}'`);
+      final dx = tidy(() => inputGradients[inputName]!());
+      if (dx.dtype != 'float32') {
+        throw Exception(
+            'Error in gradient for op ${node.kernelName}. The gradient of input ' +
+                "${inputName} must have 'float32' dtype, but has '${dx.dtype}'");
       }
-      const x = node.inputs[inputName];
+      final x = node.inputs[inputName]!;
       if (!util.arraysEqual(dx.shape, x.shape)) {
-        throw new Error(
-            `Error in gradient for op ${
-                node.kernelName}. The gradient of input ` +
-            `'${inputName}' has shape '${dx.shape}', which does not match ` +
-            `the shape of the input '${x.shape}'`);
+        throw Exception(
+            'Error in gradient for op ${node.kernelName}. The gradient of input ' +
+                "'${inputName}' has shape '${dx.shape}', which does not match " +
+                "the shape of the input '${x.shape}'");
       }
 
-      if (tensorAccumulatedGradientMap[x.id] == null) {
+      final curGradient = tensorAccumulatedGradientMap[x.id];
+      if (curGradient == null) {
         tensorAccumulatedGradientMap[x.id] = dx;
       } else {
-        const curGradient = tensorAccumulatedGradientMap[x.id];
         tensorAccumulatedGradientMap[x.id] = add(curGradient, dx);
         curGradient.dispose();
       }
