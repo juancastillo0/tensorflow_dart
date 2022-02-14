ccaecba9f997865c3531f01bdaa45146689e93c7
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-02-14 05:33:45 +0000

    wasm module setup working

--- bin/test_funcs.dart
+++ bin/test_funcs.dart
@@ -1,15 +1,25 @@
 import 'dart:io';
 
+import 'package:tensorflow_wasm/src/emscripten_module.dart';
 import 'package:wasm/wasm.dart';
 
-void main() {
+void main() async {
   final moduleBytes = File('./tfjs-backend-wasm.wasm').readAsBytesSync();
   final module = WasmModule(moduleBytes);
   print(module.describe());
-  final instance = module.builder().build();
-  print(instance.lookupFunction('init'));
-  print(instance.lookupFunction('getThreadsCount'));
-  print(instance.lookupFunction('cwrap'));
+  // final instance = module.builder().build();
+  // print(instance.lookupFunction('init'));
+  // print(instance.lookupFunction('getThreadsCount'));
+  // print(instance.lookupFunction('cwrap'));
+
+  final mod = await wasmFactory(
+    WasmFactoryConfig(wasmBinary: moduleBytes.buffer),
+  );
+  await Future.delayed(Duration(seconds: 2));
+  for (final d in mod.map.entries.where(
+      (element) => element.value is! List && element.value is! WasmMemory)) {
+    print(d);
+  }
 }
 
 // init(): void,
