937560fb6b25928844466b49afbabb0c45c1373a
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-02-17 04:02:25 +0000

    export new ops

--- lib/src/ops/ops.dart
+++ lib/src/ops/ops.dart
@@ -1,12 +1,18 @@
-export 'add.dart';
 export 'buffer.dart';
-export 'cast.dart';
 export 'clone.dart';
 export 'print.dart';
+//
 export 'tensor.dart';
+export 'scalar.dart';
+//
+export 'add.dart';
 export 'div.dart';
 export 'floor_div.dart';
-export 'scalar.dart';
+export 'squared_difference.dart';
+//
+export 'cast.dart';
 export 'stack.dart';
 export 'unstack.dart';
-export 'squared_difference.dart';
+//
+export 'logical.dart';
+export 'logical_binary.dart';
