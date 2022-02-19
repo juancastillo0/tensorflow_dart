b8fc90415a9bdcaf2c478182b24edc1d4deffdba
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-02-19 17:28:50 +0000

    register wasm kernels

--- lib/src/wasm/kernels/register_all_kernels.dart
+++ lib/src/wasm/kernels/register_all_kernels.dart
@@ -1,5 +1,16 @@
 import 'package:tensorflow_wasm/src/kernel_registry.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/all.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/any.dart';
 import 'package:tensorflow_wasm/src/wasm/kernels/base_kernels.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/concat.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/depthwise_conv2d_native.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/expand_dims.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/identity.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/max.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/pack.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/reshape.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/sum.dart';
+import 'package:tensorflow_wasm/src/wasm/kernels/transpose.dart';
 
 void registerAllKernels() {
   final configs = <KernelConfig>[
@@ -37,6 +48,37 @@ void registerAllKernels() {
     absConfig,
     sqrtConfig,
     reluConfig,
+    //
+    identityConfig,
+    allConfig,
+    anyConfig,
+    minConfig,
+    maxConfig,
+    sumConfig,
+    prodConfig,
+    //
+    transposeConfig,
+    concatConfig,
+    expandDimsConfig,
+    packConfig,
+    reshapeConfig,
+    //
+    depthwiseConv2dNativeConfig,
+    // ArgMax
+    // Cumsum
+    // FusedBatchNorm
+    // FusedConv2D
+    // FusedDepthwiseConv2D
+    // Max
+    // Min
+    // Prod
+    // Sum
+    // Mean
+    // OneHot
+    // Sigmoid
+    // Softmax
+    // TopK
+    // _FusedMatMul
   ];
 
   for (final config in configs) {
