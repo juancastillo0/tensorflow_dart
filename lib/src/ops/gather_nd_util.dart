a44fb3d1652cf9e87ff952c4a91a14a4d619e957
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-02-23 03:57:54 +0000

    gather nd util

--- lib/src/ops/gather_nd_util.dart
+++ lib/src/ops/gather_nd_util.dart
@@ -1,3 +1,8 @@
+import 'package:tensorflow_wasm/src/util_base.dart';
+import 'package:tensorflow_wasm/tensorflow_wasm.dart';
+
+import '../tensor.dart';
+
 /**
  * @license
  * Copyright 2018 Google LLC. All Rights Reserved.
@@ -14,9 +19,25 @@
  * limitations under the License.
  * =============================================================================
  */
-import {TensorInfo} from '../kernel_registry';
-import {computeStrides, sizeFromShape} from '../util';
+// import {TensorInfo} from '../kernel_registry';
+// import {computeStrides, sizeFromShape} from '../util';
 
+class GatherIndicesInfo {
+  final List<int> resultShape;
+  final int numSlices;
+  final int sliceSize;
+  final List<int> strides;
+
+  GatherIndicesInfo({
+    required this.resultShape,
+    required this.numSlices,
+    required this.sliceSize,
+    required this.strides,
+  });
+}
+
+class GatherUtil {
+  const GatherUtil._();
 /**
  * Validate gather nd inputs.
  *
@@ -25,62 +46,66 @@ import {computeStrides, sizeFromShape} from '../util';
  *
  * @returns [resultShape, numUpdates, sliceSize, strides]
  */
-export function prepareAndValidate(tensor: TensorInfo, indices: TensorInfo):
-    [number[], number, number, number[]] {
-  const tensorRank = tensor.shape.length;
-  const indicesRank = indices.shape.length;
-  if (tensorRank < 1) {
-    throw new Error(
-        'tf.gatherND() expects the input to be rank 1 or higher,' +
-        ` but the rank was ${tensorRank}.`);
+  static GatherIndicesInfo prepareAndValidate(
+      TensorInfo tensor, TensorInfo indices) {
+    final tensorRank = tensor.shape.length;
+    final indicesRank = indices.shape.length;
+    if (tensorRank < 1) {
+      throw Exception(
+          'tf.gatherND() expects the input to be rank 1 or higher,' +
+              ' but the rank was ${tensorRank}.');
+    }
+    if (indicesRank < 1) {
+      throw Exception(
+          'tf.gatherND() expects the indices to be rank 1 or higher,' +
+              ' but the rank was ${indicesRank}.');
+    }
+    if (indices.dtype != 'int32') {
+      throw Exception('tf.gatherND() expects the indices to be int32 type,' +
+          ' but the dtype was ${indices.dtype}.');
+    }
+    if (indices.shape[indicesRank - 1] > tensorRank) {
+      throw Exception(
+          'index innermost dimension length must be <= tensor rank; saw: ' +
+              '${indices.shape[indicesRank - 1]} vs. ${tensorRank}');
+    }
+
+    if (sizeFromShape(tensor.shape) == 0) {
+      throw Exception('Requested more than 0 entries, but input is empty.' +
+          ' Input shape: ${tensor.shape}.');
+    }
+
+    final indicesShape = indices.shape;
+    final sliceRank = indicesShape[indicesShape.length - 1];
+
+    // The result shape is
+    //   indices.shape[:-1] + params.shape[indices.shape[-1]:]
+    int nResult = 1;
+    for (int i = 0; i < indicesShape.length - 1; ++i) {
+      nResult *= indicesShape[i];
+    }
+
+    final inputShape = tensor.shape;
+
+    final resultShape = [...indicesShape];
+    resultShape.removeLast();
+
+    int sliceSize = 1;
+    for (int i = sliceRank; i < tensorRank; ++i) {
+      sliceSize *= inputShape[i];
+      resultShape.add(inputShape[i]);
+    }
+
+    final strides = [
+      ...computeStrides(tensor.shape).map((stride) => stride ~/ sliceSize),
+      1
+    ].slice(0, sliceRank);
+
+    return GatherIndicesInfo(
+      resultShape: resultShape,
+      numSlices: nResult,
+      sliceSize: sliceSize,
+      strides: strides,
+    );
   }
-  if (indicesRank < 1) {
-    throw new Error(
-        'tf.gatherND() expects the indices to be rank 1 or higher,' +
-        ` but the rank was ${indicesRank}.`);
-  }
-  if (indices.dtype !== 'int32') {
-    throw new Error(
-        'tf.gatherND() expects the indices to be int32 type,' +
-        ` but the dtype was ${indices.dtype}.`);
-  }
-  if (indices.shape[indicesRank - 1] > tensorRank) {
-    throw new Error(
-        'index innermost dimension length must be <= tensor rank; saw: ' +
-        `${indices.shape[indicesRank - 1]} vs. ${tensorRank}`);
-  }
-
-  if (sizeFromShape(tensor.shape) === 0) {
-    throw new Error(
-        'Requested more than 0 entries, but input is empty.' +
-        ` Input shape: ${tensor.shape}.`);
-  }
-
-  const indicesShape = indices.shape;
-  const sliceRank = indicesShape[indicesShape.length - 1];
-
-  // The result shape is
-  //   indices.shape[:-1] + params.shape[indices.shape[-1]:]
-  let nResult = 1;
-  for (let i = 0; i < indicesShape.length - 1; ++i) {
-    nResult *= indicesShape[i];
-  }
-
-  const inputShape = tensor.shape;
-
-  const resultShape = indicesShape.slice();
-  resultShape.pop();
-
-  let sliceSize = 1;
-  for (let i = sliceRank; i < tensorRank; ++i) {
-    sliceSize *= inputShape[i];
-    resultShape.push(inputShape[i]);
-  }
-
-  const strides =
-      [...computeStrides(tensor.shape).map(stride => stride / sliceSize),
-       1].slice(0, sliceRank);
-
-  return [resultShape, nResult, sliceSize, strides];
 }
-
