a67af7a2bea6fa0b05593259796c1f42063866a6
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-03-11 03:07:46 +0000

    native example

--- example/tensorflow_wasm_example.dart
+++ example/tensorflow_wasm_example.dart
@@ -1,6 +1,40 @@
-import 'package:tensorflow_wasm/tensorflow_wasm.dart';
+import 'dart:io';
 
-void main() {
-  // var awesome = Awesome();
-  // print('awesome: ${awesome.isAwesome}');
+import 'package:path/path.dart';
+import 'package:tensorflow_wasm/backend_wasm.dart';
+import 'package:tensorflow_wasm/converter.dart';
+import 'package:tensorflow_wasm/tensorflow_wasm.dart' as tf;
+
+void main() async {
+  final modelUrl = joinAll([
+    '/',
+    ...Platform.script.pathSegments.sublistRelaxed(0, -1),
+    'model',
+    'model.json'
+  ]);
+
+  setWasmPaths(
+    joinAll([
+      '/',
+      ...Platform.script.pathSegments.sublistRelaxed(0, -2),
+      'bin/',
+    ]),
+  );
+  await tf.setBackend(wasmBackendFactory);
+  final model = await loadGraphModel(ModelHandler.fromUrl('file://$modelUrl'));
+
+  print(model.inputs);
+  print(model.metadata);
+  print(model.outputs);
+
+  final result = await model.executeAsync(tf.TensorList([
+    tf.tensor([
+      [0.1, 0.2, 0.3, 0.4, 0]
+    ]),
+    tf.ones([1, 28, 28, 1]),
+    tf.scalar(false),
+  ]));
+
+  print(result.toTensorList());
+  print(result.toTensorList().first.dataSync());
 }
